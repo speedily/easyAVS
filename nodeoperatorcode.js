81a18b243aa3a4c1c0b80c621952b2c3ccf31a48
speedily <speedily@users.noreply.github.com>
2024-10-31 21:42:01 +0530

    nodeoperatorcode.js

---
+++ nodeoperatorcode.js
@@ -0,0 +1,18 @@
+const Web3 = require('web3');
+
+const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+
+const contractAddress = 'YOUR_CONTRACT_ADDRESS';
+const contractABI = [/* Your contract's ABI */];
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+async function mintTokens() {
+  const accounts = await web3.eth.getAccounts();
+  const account = accounts[0];
+
+  const tx = await contract.methods.mintToken(100).send({ from: account });
+  console.log('Tokens minted:', tx.transactionHash);
+}
+
+mintTokens();
